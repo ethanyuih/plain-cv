d6392349b0791b3fc93e0de6d7d4d332e2233849
Easian Yu <easianyu@gmail.com>
2024-12-31 13:31:52 -0800

    update
    (cherry picked from commit b824054b7709746dfc99d589ae1a6efe909e2415)

--- src/components/plaintext.component.tsx
+++ src/components/plaintext.component.tsx
@@ -1,6 +1,6 @@
-import { FC } from 'react';
-import styled from 'styled-components';
-import pageConfig from '../assets/resume-infos/0-page-setting';
+import { FC } from "react";
+import styled from "styled-components";
+import pageConfig from "../assets/resume-infos/0-page-setting";
 
 type Props = {
   text: string;
@@ -13,32 +13,33 @@ const PlainTextContainer = styled.div`
 const PlainText: FC<Props> = ({ text }) => {
   return (
     <PlainTextContainer>
-      {text.split(' ').map((word, idx) => {
-        if (word[0] === '#' && word[word.length - 1] === '#') {
-          return (
-            <strong key={idx}>
-              {word.substring(1, word.length - 1)}
-              {` `}
-            </strong>
-          );
-        }
+      {text}
+      {/*{text.split(' ').map((word, idx) => {*/}
+      {/*  if (word[0] === '#' && word[word.length - 1] === '#') {*/}
+      {/*    return (*/}
+      {/*      <strong key={idx}>*/}
+      {/*        {word.substring(1, word.length - 1)}*/}
+      {/*        {` `}*/}
+      {/*      </strong>*/}
+      {/*    );*/}
+      {/*  }*/}
 
-        if (word[0] === '^' && word[word.length - 1] === '^') {
-          return (
-            <em key={idx}>
-              {word.substring(1, word.length - 1)}
-              {` `}
-            </em>
-          );
-        }
+      {/*  if (word[0] === '^' && word[word.length - 1] === '^') {*/}
+      {/*    return (*/}
+      {/*      <em key={idx}>*/}
+      {/*        {word.substring(1, word.length - 1)}*/}
+      {/*        {` `}*/}
+      {/*      </em>*/}
+      {/*    );*/}
+      {/*  }*/}
 
-        return (
-          <span key={idx}>
-            {word}
-            {` `}
-          </span>
-        );
-      })}
+      {/*  return (*/}
+      {/*    <span key={idx}>*/}
+      {/*      {word}*/}
+      {/*      {` `}*/}
+      {/*    </span>*/}
+      {/*  );*/}
+      {/*})}*/}
     </PlainTextContainer>
   );
 };
