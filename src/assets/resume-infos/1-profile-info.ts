f8304b9422193202b0bfaf8be6d92e07084719d4
YyyyXx <yixuanyu98@gmail.com>
2024-12-31 02:43:11 -0800

    update

--- src/assets/resume-infos/1-profile-info.ts
+++ src/assets/resume-infos/1-profile-info.ts
@@ -4,7 +4,7 @@ export const profileInfo: Profile = {
   name: "Ethan Yu",
   phone: "(626)390-2933",
   email: "ethanyyou@gmail.com",
-  linkedin: "linkedin.com/in/ethanyyou",
+  linkedin: "https://www.linkedin.com/in/ethanyyou",
   github: "github.com/ethanyyou",
   // address: '153 S Muscatel Ave, San Gabriel, CA, 91776',
   address: "Los Angeles, California (Willing to relocate)",
