7a215c9790c5eb547ec2ab022d5d776c31a5dc85
Easian Yu <easianyu@gmail.com>
2024-12-31 15:13:13 -0800

    ~

--- src/assets/resume-infos/1-profile-info.ts
+++ src/assets/resume-infos/1-profile-info.ts
@@ -7,7 +7,7 @@ export const profileInfo: Profile = {
   phone: "(626)390-2933",
   email: "ethanyyou@gmail.com",
   linkedin: "https://www.linkedin.com/in/ethanyyou",
-  github: "github.com/ethanyyou",
+  github: "https://www.github.com/ethanyyou",
   // address: '153 S Muscatel Ave, San Gabriel, CA, 91776',
   address: "Los Angeles, California (Willing to relocate)",
   homeState: "California",
